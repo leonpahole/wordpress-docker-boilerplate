aa06bdaeb2560fa1f21b8b8ed37386fd38475e7a
Leon Pahole <leonnieen@gmail.com>
2020-03-19 22:59:27 +0100

    Explicitly use www-data in local permissions script

--- fix_permissions_for_local_development.sh
+++ fix_permissions_for_local_development.sh
@@ -2,7 +2,7 @@
 
 # try getting uid from docker, if it fails, try 33 (should work)
 WWW_DATA_UID=33
-RESULT=$(docker-compose exec wordpress id -u)
+RESULT=$(docker-compose exec -u www-data wordpress id -u)
 COMMAND_SUCCESS=$?
 if [ $COMMAND_SUCCESS -eq 0 ]; then
   WWW_DATA_UID=$(echo $RESULT | tr -d '\r')
